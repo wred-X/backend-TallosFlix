83e56f2a51726be0bfbbc5f2921f69ebeaa0f746
Wesley Romão <wesleyredx@gmail.com>
2022-09-06 10:42:36 -0300

    documentando o schema de sessions

--- src/sessions/shared/session.ts
+++ src/sessions/shared/session.ts
@@ -1,8 +1,21 @@
-import { Document } from 'mongoose';
 import { ApiProperty } from '@nestjs/swagger';
-// import { Expose } from 'class-transformer';
+import { IsNotEmpty } from 'class-validator';
 
 export class Session {
+  @ApiProperty({
+    example: 'ID1234',
+    description: `ID do user que entrou em sessão`,
+  })
+  @IsNotEmpty({
+    message: 'ID do user que entrou em sessão é obrigatório.',
+  })
   user_id: string;
+  @ApiProperty({
+    example: 'eyJWTEXAMPLEasjosdnaos13rr423423423JWTEXAMPLEJWTEXAMPLE',
+    description: `O jwt da sessão atual do usuario.`,
+  })
+  @IsNotEmpty({
+    message: 'jwt é obrigatório.',
+  })
   jwt: string;
 }
