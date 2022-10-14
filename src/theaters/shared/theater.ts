146a64e94c1aa805ed1443e5d92f6ef0c2f92491
Juliana Oliveira <juliana.oliveira@tallos.com.br>
2022-10-14 12:26:08 -0300

    test service theater

--- src/theaters/shared/theater.ts
+++ src/theaters/shared/theater.ts
@@ -3,6 +3,26 @@ import { IsNotEmpty } from 'class-validator';
 import { Document } from 'mongoose';
 
 export class Theater {
+  constructor(Theater?: Partial<Theater>) {
+    this.theaterId = Theater?.theaterId,
+    this.location = Theater?.location,
+    this.location.address = Theater?.location.address,
+    this.location.address.street1 = Theater?.location.address.street1,
+    this.location.address.city = Theater?.location.address.city,
+    this.location.address.state = Theater?.location.address.state,
+    this.location.address.zipcode = Theater?.location.address.zipcode,
+    this.location.geo.type = Theater?.location.geo.type,
+    this.location.geo.coordinates = Theater?.location.geo.coordinates,
+    this._id = Theater?._id
+
+
+  }
+
+  @ApiProperty({
+    example: '1232fkk5',
+    description: `_id do teatro`,
+  })
+  _id?: string;
   @ApiProperty({
     example: 20000,
     description: `ID do teatro`,
@@ -23,9 +43,11 @@ export class Theater {
     };
     geo: {
       type: string;
-      coordinates: [number];
+      coordinates: number[];
     };
   };
+
+
 }
 
 // export interface Coordinates  {
