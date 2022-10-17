4286de97ee693573a351315620cfb36d1d9c03f9
Lucas <lucas.tallos@tallos.com.br>
2022-10-17 10:30:34 -0300

    feat: Criando test do getAll commentController

--- src/comments/comments.controller.spec.ts
+++ src/comments/comments.controller.spec.ts
@@ -1,18 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentsController } from './comments.controller';
+import { CommentService } from './shared/comment.service';
+import { Comment } from './shared/comment';
 
+const CommentsList: Comment[] = [
+  new Comment({ name: 'Lucas', email: 'lucas@gmail.com', movie_id: '573a1390f29313caabcd41b1', text: 'Nunca havia assistido,filme muito bom', date: new Date('1988-10-16T19:08:23.000Z') }),
+  new Comment({ name: 'Wesley', email: 'Wesley@gmail.com', movie_id: '573a1390f29313caabcd4218', text: 'Filme deixou a desejar', date: new Date('1988-10-16T19:08:23.000Z') }),
+  new Comment({ name: 'Juliana', email: 'Juliana@gmail.com', movie_id: '573a1390f29313caabcd432a', text: 'filme muito bom', date: new Date('1988-10-16T19:08:23.000Z') })
+]
+const comment = new Comment({
+  name: 'Pedro', email: 'pedrinDoGrau@gmail.com', movie_id: '573a1390f29313caabcd41b1', text: 'Filme muito ruim filho', date: new Date('1988-10-16T19:08:23.000Z')
+})
 describe('CommentsController', () => {
-  let controller: CommentsController;
+  let commentsController: CommentsController;
+  let commentsService: CommentService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommentsController],
+      providers:[{
+        provide: CommentService,
+        useValue:{
+          getAll: jest.fn().mockRejectedValue(CommentsList)
+        }
+      }]
     }).compile();
 
-    controller = module.get<CommentsController>(CommentsController);
+    commentsController = module.get<CommentsController>(CommentsController);
+    commentsService = module.get<CommentService>(CommentService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(commentsController).toBeDefined();
   });
+  describe('GetAll', ()=>{
+    it('Deve retornar uma lista', async ()=>{
+      try {
+        const result = await commentsController.getAll();
+        expect(result).toEqual(CommentsList);
+        expect(typeof result).toEqual('object');    
+      } catch (error) {
+        console.log('Error >>>>', error)
+      }
+      })
+  })
 });
