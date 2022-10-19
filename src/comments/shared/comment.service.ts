70766ebfc16e89f7830b0a70fda4cac2dbb002ac
Wesley Romão <wesleyredx@gmail.com>
2022-10-19 12:04:56 -0300

    padronização de erros em comments

--- src/comments/shared/comment.service.ts
+++ src/comments/shared/comment.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Comment } from './comment';
@@ -18,20 +18,28 @@ export class CommentService {
     const total = await this.commentsModel.countDocuments(comment);
     const qtdPages = Math.floor(total / pagination.limit) + 1;
 
-    const response = await this.commentsModel
-      .find(comment)
-      .limit(limit)
-      .skip(skip);
-    return {
-      response,
-      numberOfElements: total,
-      pagesTotal: qtdPages,
-      page: pagination.page || 1,
-    };
+    try {
+      const response = await this.commentsModel
+        .find(comment)
+        .limit(limit)
+        .skip(skip);
+      return {
+        response,
+        numberOfElements: total,
+        pagesTotal: qtdPages,
+        page: pagination.page || 1,
+      };
+    } catch {
+      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+    }
   }
 
   async getById(id: string) {
-    return await this.commentsModel.findById(id).exec();
+    try {
+      return await this.commentsModel.findById(id).exec();
+    } catch {
+      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+    }
   }
 
   // async getByMovieId(movie_id: string) {
@@ -41,34 +49,55 @@ export class CommentService {
 
   async getByMovieId(movie_id: string) {
     const id = new ObjectId(movie_id);
-    const commentsMovie = await this.commentsModel
-      .find({ movie_id: id })
-      .limit(50);
-    return commentsMovie;
+    try {
+      const commentsMovie = await this.commentsModel
+        .find({ movie_id: id })
+        .limit(50);
+      return commentsMovie;
+    } catch {
+      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+    }
   }
 
   //criar paginação de comentarios do filme
 
   async getByEmail(mail: string) {
-    const commentsMovie = await this.commentsModel
-      .find({ email: mail })
-      .limit(50);
-    return commentsMovie;
+    try {
+      const commentsMovie = await this.commentsModel
+        .find({ email: mail })
+        .limit(50);
+      return commentsMovie;
+    } catch {
+      throw new HttpException('Check data -mail- ', HttpStatus.NOT_ACCEPTABLE);
+    }
   }
 
   async create(comments: Comment): Promise<Comment> {
-    const createdComment = new this.commentsModel(comments);
-    this.socket.emitNewComment(createdComment);
-    return await createdComment.save();
+    try {
+      const createdComment = new this.commentsModel(comments);
+      this.socket.emitNewComment(createdComment);
+      return await createdComment.save();
+    } catch {
+      throw new HttpException('Check all datas', HttpStatus.NOT_ACCEPTABLE);
+    }
   }
 
   async update(id: string, comments: Comment) {
-    return await this.commentsModel.findByIdAndUpdate(id, comments, {
-      new: true,
-    });
+    try {
+      const uppdate = await this.commentsModel.findByIdAndUpdate(id, comments, {
+        new: true,
+      });
+      return uppdate;
+    } catch {
+      throw new HttpException('Check all datas', HttpStatus.NOT_ACCEPTABLE);
+    }
   }
 
   async delete(id: string) {
-    return await this.commentsModel.findByIdAndDelete({ _id: id }).exec();
+    try {
+      return await this.commentsModel.findByIdAndDelete({ _id: id }).exec();
+    } catch {
+      throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    }
   }
 }
