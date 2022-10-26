e0522d70030377a1b77a7c75a3af6392423fcd5f
juhdeveloper <juhdeveloper@gmail.com>
2022-10-26 17:41:00 -0300

    ok

--- src/likes/shared/likes.service.ts
+++ src/likes/shared/likes.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { count } from 'console';
 import { Model } from 'mongoose';
 import { Likes } from '../model/likes';
-
+import  ObjectId, {Types} from 'mongoose';
 @Injectable()
 export class LikesService {
   @InjectModel('Likes') private readonly likesModel: Model<Likes>;
@@ -14,16 +15,17 @@ export class LikesService {
 
   async likeComment(id: string, liked: Likes) {
     const userLike = liked.userLike;
-    console.log(userLike);
+
     //verifica se o userId ja deu like ou deslike nesse comentario especifico
     const validateLiked = await this.likesModel.findOne({
       commentId: id,
       'userLike.userId': userLike.userId,
     });
     //caso tenha dado like ou unlike, ele fara o update do novo like ou unlike
+
+
     if (validateLiked) {
-      console.log('sim');
-      console.log(liked.userLike);
+      console.log('sim')
       await this.likesModel.findOneAndUpdate(
         { commentId: id, 'userlike.userId': userLike.userId },
         {
@@ -36,12 +38,16 @@ export class LikesService {
       this.pushLiked(id, liked);
     } else {
       //caso n tenha dado like e nem unlike, n existe o objeto dentro do array de userLikes, então ele deve criar um novo objeto dentro do array do commentId
+
       this.pushLiked(id, liked);
       //await this.likesModel.create(liked);
     }
     return validateLiked;
   }
+
+  
   async pushLiked(id: string, liked: Likes) {
+    console.log('deu bom')
     const pushLike = await this.likesModel.findOneAndUpdate(
       { commentId: id },
       { $push: { userLike: liked.userLike } },
