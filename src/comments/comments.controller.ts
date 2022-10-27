d1c0673bf475ef63486a85b17d8b93c4fdf13b53
Lucas <lucas.tallos@tallos.com.br>
2022-10-27 15:34:43 -0300

    feat: paginacao no retorno do comments movie

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -41,8 +41,8 @@ export class CommentsController {
   @IsPublic()
   @ApiBody({ type: MovieId })
   @Post('movie_id')
-  async getByMovieId(@Body() movie_id: { movie: string }): Promise<Comment[]> {
-    const comments = await this.commentService.getByMovieId(movie_id.movie);
+  async getByMovieId(@Query() pagination,@Body() movie_id: { movie: string }): Promise<Comment[]> {
+    const comments = await this.commentService.getByMovieId(pagination,movie_id.movie);
     return comments;
   }
   @IsPublic()
