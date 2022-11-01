420f7a40e00bcc4c9060ad0416fb7adea490b74c
Wesley Romão <wesleyredx@gmail.com>
2022-11-01 10:48:21 -0300

    movie id string para object id

--- src/ratings/schemas/ratings.schema.ts
+++ src/ratings/schemas/ratings.schema.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 
 export const RatingSchema = new mongoose.Schema({
-  movie_id: String,
+  movie_id: mongoose.Schema.Types.ObjectId,
   allRate: {
     type: [Object],
     required: true,
