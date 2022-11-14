703408268b43f5587927795fcf35a3d1dd30f1b9
juhdeveloper <juhdeveloper@gmail.com>
2022-11-14 16:04:05 -0300

    descomentando dtos

--- src/ratings/shared/rating.ts
+++ src/ratings/shared/rating.ts
@@ -6,23 +6,23 @@ export class Rating {
   @IsOptional()
   _id?: string;
 
-  // @ApiProperty({
-  //   example: '1movieID1',
-  //   description: `Id para referenciar o filme a qual pertence a nota.`,
-  // })
-  // @IsNotEmpty({
-  //   message: 'Id é obrigatorio para saber a quem pertence a nota.',
-  // })
+  @ApiProperty({
+    example: '1movieID1',
+    description: `Id para referenciar o filme a qual pertence a nota.`,
+  })
+  @IsNotEmpty({
+    message: 'Id é obrigatorio para saber a quem pertence a nota.',
+  })
   movie_id: string;
 
-  // @ApiProperty({
-  //   example: [{ rate: 5, user_id: 'idDEusuario123' }],
-  //   description: `Id dos filmes adicionado aos favoritos desse usuario.`,
-  // })
-  // @IsNotEmpty({
-  //   message:
-  //     'Id do filme é obrigatorio para ser adicionado como favorito do usuario.',
-  // })
+  @ApiProperty({
+    example: [{ rate: 5, user_id: 'idDEusuario123' }],
+    description: `Id dos filmes adicionado aos favoritos desse usuario.`,
+  })
+  @IsNotEmpty({
+    message:
+      'Id do filme é obrigatorio para ser adicionado como favorito do usuario.',
+  })
   allRate: Rate[];
 
   constructor(todo?: Partial<Rating>) {
