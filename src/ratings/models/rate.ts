32834b7b6ee8245339de8131f23def8f6e44d4a8
juhdeveloper <juhdeveloper@gmail.com>
2022-11-14 16:01:56 -0300

    ajustado rota add/id para buscar o movie_id

--- src/ratings/models/rate.ts
+++ src/ratings/models/rate.ts
@@ -2,22 +2,22 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class Rate {
-  @ApiProperty({
-    example: 5,
-    description: `nota desse usuario.`,
-  })
-  @IsNotEmpty({
-    message: 'nota é obrigatoria para ser gerada avaliação.',
-  })
+  // @ApiProperty({
+  //   example: 5,
+  //   description: `nota desse usuario.`,
+  // })
+  // @IsNotEmpty({
+  //   message: 'nota é obrigatoria para ser gerada avaliação.',
+  // })
   rate: number;
 
-  @ApiProperty({
-    example: '1userID1',
-    description: `Id para referenciar a qual user pertence a nota.`,
-  })
-  @IsNotEmpty({
-    message: 'Id do user é obrigatorio para saber a qual user pertence a nota.',
-  })
+  // @ApiProperty({
+  //   example: '1userID1',
+  //   description: `Id para referenciar a qual user pertence a nota.`,
+  // })
+  // @IsNotEmpty({
+  //   message: 'Id do user é obrigatorio para saber a qual user pertence a nota.',
+  // })
   user_id: string;
 
   constructor(todo?: Partial<Rate>) {
